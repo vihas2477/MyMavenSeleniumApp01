29f708d0df0f8ac12b77d2071411067af06b29f1
B VIHAS <149416238+vihas2477@users.noreply.github.com>
2026-04-17 13:32:02 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,23 +3,39 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver=new ChromeDriver();
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Automatically download and setup ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        // Headless mode for Jenkins
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        thread.sleep(5000);
+        Thread.sleep(2000);
+
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        thread.sleep(5000);
+        Thread.sleep(2000);
+
         driver.findElement(By.id("login-button")).click();
-        thread.sleep(5000);
+        Thread.sleep(5000);
+
+        System.out.println("Test executed successfully!");
+
+        driver.quit();
     }
 }
